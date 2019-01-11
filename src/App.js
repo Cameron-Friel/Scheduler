b2612e4d28c09a56fc6022ecf070d78f4832ff16
Cameron-Friel <frielc@oregonstate.edu>
2019-01-10 18:01:10 -0800

    Adds routing to list item navigation tabs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 import { ListItem } from './components';
+import { Home, Schedules } from './pages';
 
 class App extends Component {
+  /**
+    * Renders the current view of the desktop app
+  **/
+
+  renderView = () => {
+    return (
+      <this.props.view />
+    );
+  }
+
   render() {
     return (
       <div className='container'>
@@ -12,13 +24,21 @@ class App extends Component {
 
         <div className='content-window'>
           <div className='vertical-nav-bar'>
-            <ListItem text={ 'Home' } />
-            <ListItem text={ 'Schedules' } />
+            <ListItem text={ 'Home' } newView={ Home } />
+            <ListItem text={ 'Schedules' } newView={ Schedules } />
           </div>
+
+          { this.renderView() }
         </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    view: state.AppReducer.view,
+  };
+};
+
+export default connect(mapStateToProps, {}) (App);
---
+++ src/pages/Schedules.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Schedules extends Component {
+  render() {
+    return (
+      <div>Schedule Page</div>
+    );
+  }
+}
+
+export default Schedules;
