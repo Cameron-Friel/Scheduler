62801837074d2eb0ae88faef63927fd89dcae340
Cameron-Friel <frielc@oregonstate.edu>
2019-03-04 22:19:07 -0800

    Adds the Modal Component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
 export const UPDATE_VIEW='UPDATE_VIEW';
+export const SET_MODAL_FOCUS='SET_MODAL_FOCUS';
 
 export const updateView = (view) => ({
   type: UPDATE_VIEW,
   payload: view,
+});
+
+export const setModalFocus = (bool) => ({
+  type: SET_MODAL_FOCUS,
+  payload: bool,
 });
