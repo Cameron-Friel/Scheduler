4e5605e6941d0f1c63a5fe287fd9a8cefd778006
Cameron-Friel <frielc@oregonstate.edu>
2019-03-04 18:35:22 -0800

    Removes uneeded code

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -4,30 +4,6 @@ import { connect } from 'react-redux';
 import { updateView } from '../actions';
 
 class AppButton extends Component {
-  /**
-    * Checks if a view in the app is already in use or not
-    * @param {string} newView - The view the user wishes to see
-    * @returns {boolean}
-  **/
-
-  isDuplicateView = (newView) => {
-    if (this.props.view === newView) {
-      return true;
-    }
-    return false;
-  }
-
-  /**
-    * Updates the view the user is seeing based on the list item they clicked
-    * @param {string} view - The view the user wishes to see
-  **/
-
-  updateView = (newView) => {
-    if (!this.isDuplicateView(newView)) {
-      this.props.updateView(newView);
-    }
-  }
-
   render() {
     return (
       <div className='button-container' onClick={this.props.clickHandler}><p className='main-text button-text'>{ this.props.buttonName }</p></div>
@@ -37,8 +13,8 @@ class AppButton extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    view: state.AppReducer.view,
+
   };
 };
 
-export default connect(mapStateToProps, { updateView }) (AppButton);
+export default connect(mapStateToProps, { }) (AppButton);
